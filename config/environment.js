01d6151d6a602fd8a2bb6e16147643ea03aeb9e2
Nick Schot <nickschot@gmail.com>
2018-10-28 12:40:36 +0100

    rename app to ember-training

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'ember-lts-app',
+    modulePrefix: 'ember-training',
     environment,
     rootURL: '/',
     locationType: 'auto',
