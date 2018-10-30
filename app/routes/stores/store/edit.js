b41b512864b4dca5e993828472875f05cddba5b2
Casper Smits <casper@caspersmits.nl>
2018-10-30 15:57:31 +0100

    Fix edit route

--- app/routes/stores/store/edit.js
+++ app/routes/stores/store/edit.js
@@ -3,9 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   actions: {
     willTransition(transition){
-      if(this.controller.model.isNew || this.controller.model.hasDirtyAttributes){
+      if(this.controller.model.hasDirtyAttributes){
         if (confirm('Are you sure you want to leave?')){
-          this.controller.model.deleteRecord();
+          this.controller.model.rollbackAttributes();
         } else {
           transition.abort();
         }
