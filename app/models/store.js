41fa01195371c944a1a3362b2770aea4a339d277
Nick Schot <nickschot@gmail.com>
2018-10-30 11:45:08 +0100

    add some validations to store model

--- app/models/store.js
+++ app/models/store.js
@@ -2,7 +2,30 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
 
-export default Model.extend({
+import { validator, buildValidations } from 'ember-cp-validations';
+
+const Validations = buildValidations(
+  {
+    name: {
+      description: 'Name',
+      validators: [
+        validator('presence', true),
+        validator('length', {
+          min: 3,
+          max: 16
+        })
+      ]
+    },
+    address: {
+      description: 'Address',
+      validators: [
+        validator('presence', true),
+      ]
+    }
+  }
+);
+
+export default Model.extend(Validations, {
   name: attr(),
   address: attr(),
 
