698b921f5d2db7f454a39b573a28b6bf3031609e
Casper Smits <casper@caspersmits.nl>
2018-12-15 14:20:39 +0100

    Add input integration test

--- tests/integration/components/search-input-test.js
+++ tests/integration/components/search-input-test.js
@@ -1,26 +1,21 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | search-input', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{search-input}}`);
+    await render(hbs`<SearchInput />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
-    await render(hbs`
-      {{#search-input}}
-        template block text
-      {{/search-input}}
-    `);
+    this.set('title', 'Some weird title');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(hbs`<SearchInput @value={{title}} @onChange={{action (mut title)}} />`);
+    await fillIn('input', 'You are not a wizard!');
+
+    assert.equal(this.get('title'), 'You are not a wizard!');
   });
 });
