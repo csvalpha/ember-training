198a22771a234c8744d34e3dd4290ce54aa9c55b
Casper Smits <casper.smits@nedap.com>
2019-09-29 12:40:43 +0200

    Connect to https version of demo api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
 
 export default DS.JSONAPIAdapter.extend(AdapterFetch, {
-  host: 'http://jsonapiplayground.reyesoft.com',
+  host: 'https://jsonapiplayground.reyesoft.com',
   namespace: 'v2'
 });
