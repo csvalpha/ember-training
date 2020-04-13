8618a5e044b527ed3c4ec8d12a519119752f32fb
Casper Smits <casper.smits@nedap.com>
2020-04-13 15:04:25 +0200

    Refactor some syntax into oneliners

--- app/router.js
+++ app/router.js
@@ -10,20 +10,16 @@ Router.map(function() {
   this.route('resources');
 
   this.route('authors', function() {
-    this.route('author', {
-      path: ':author_id'
-    });
+    this.route('author', { path: ':author_id' });
   });
+
   this.route('books', function() {
-    this.route('book', {
-      path: ':book_id'
-    });
+    this.route('book', { path: ':book_id' });
   });
+
   this.route('stores', function() {
     this.route('add');
-    this.route('store', {
-      path: ':store_id'
-    }, function() {
+    this.route('store', { path: '/:store_id' }, function() {
       this.route('edit');
     });
   });
